391b8d6b26ff9d7f3ea88f4d5c05131a3bb9c4ba
jmp0422 <rhkdlf6587@naver.com>
2024-07-18 09:32:15 +0900

    2024.07.18 payment-dev

---
+++ src/main/java/com/multi/laptellect/payment/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.multi.laptellect.payment.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("/payment")
+    public void paymentpage(){
+
+    }
+}
