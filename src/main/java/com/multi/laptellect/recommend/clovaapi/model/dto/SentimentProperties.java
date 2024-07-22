80c563e84362b877beaf8250dc17dba9ba12cd0b
Youn Sang Hyun <younsang523@gmail.com>
2024-07-23 06:48:00 +0900

    커밋
    커밋

--- src/main/java/com/multi/laptellect/recommend/clovaapi/model/dto/SentimentProperties.java
+++ src/main/java/com/multi/laptellect/recommend/clovaapi/model/dto/SentimentProperties.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties(prefix = "sentiment")
+@ConfigurationProperties(prefix = "sentiment") //application.properties에 있는 sentiment로 시작하는 값들을 가져옴
 public class SentimentProperties {
 
     private String clientId;
