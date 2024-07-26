68e67dadeb880651914d099d229befc3bb45cdb2
jmp0422 <rhkdlf6587@naver.com>
2024-07-26 16:15:03 +0900

    2024.07.26 feat:payment

--- src/main/java/com/multi/laptellect/payment/controller/PaymentController.java
+++ src/main/java/com/multi/laptellect/payment/controller/PaymentController.java
@@ -1,26 +1,21 @@
 package com.multi.laptellect.payment.controller;
 
-import com.multi.laptellect.payment.model.dto.InsertDTO;
-import com.multi.laptellect.payment.model.dto.OrderlistDTO;
-import com.multi.laptellect.payment.model.dto.TestDTO;
-import com.multi.laptellect.payment.model.dto.VerificationRequestDTO;
+import com.multi.laptellect.payment.model.dto.*;
 import com.multi.laptellect.payment.service.PaymentService;
 import com.multi.laptellect.payment.service.TestService;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/payment")
@@ -28,44 +23,51 @@ public class PaymentController {
 
     private final PaymentService paymentService;
     private final TestService testService;
-    private InsertDTO insertDTO;
-    private TestDTO testDTO;
-    private OrderlistDTO orderlistDTO;
 
-    @Autowired
+
     public PaymentController(PaymentService paymentService, TestService testService) {
         this.paymentService = paymentService;
         this.testService = testService;
-        this.insertDTO = new InsertDTO();
     }
-
 
     @GetMapping("/orderlist")
     public String orderList(Model model) {
         List<OrderlistDTO> orderList = testService.selectAllOrders();
-        System.out.println("1" + orderList);
+        List<String> reviewedOrders = paymentService.getReviewedOrders();
         model.addAttribute("orderList", orderList);
+        model.addAttribute("reviewedOrders", reviewedOrders);
         return "/payment/orderlist";
     }
 
     @GetMapping("/payment")
-    public String selectTest(Model model) {
-        testDTO = testService.selectTest();
+    public String selectTest(Model model) { //로그인된 유저의 이름으로 바꾸기
+        TestDTO testDTO = testService.selectTest();
+        PaymentpointDTO paymentpointDTO = paymentService.selectpoint();
         model.addAttribute("testDTO", testDTO);
-        System.out.println(testDTO);
+        model.addAttribute("paymentpointDTO", paymentpointDTO);
+
         return "/payment/payment";
     }
 
+    @Transactional
     @PostMapping("/verifyPayment")
     public ResponseEntity<Map<String, Object>> verifyPayment(@RequestBody VerificationRequestDTO request) {
         try {
+            TestDTO testDTO = testService.selectTest();
+            InsertDTO insertDTO = new InsertDTO();
             insertDTO.setUsername2(testDTO.getUsername1());
             insertDTO.setProductinfo2(testDTO.getProductinfo1());
             insertDTO.setProductname2(testDTO.getProductname1());
-            insertDTO.setProductprice2(testDTO.getProductprice1()); // 할인금액
+            insertDTO.setProductprice2(request.getAmount().intValue());
             insertDTO.setImd2(request.getImpUid());
 
             boolean verified = paymentService.verifyPayment(request, insertDTO);
+            System.out.println(request.getUsedPoints());
+
+            PaymentpointDTO paymentpointDTO = paymentService.selectpoint();
+            paymentpointDTO.setUsedPoints(request.getUsedPoints());
+            if(Integer.parseInt(paymentpointDTO.getUsedPoints()) > 0)
+                paymentService.usepoint(paymentpointDTO);
 
             if (verified) {
                 return ResponseEntity.ok(Map.of("success", true, "message", "Payment verified successfully"));
@@ -96,5 +98,22 @@ public class PaymentController {
         } catch (IamportResponseException | IOException e) {
             return ResponseEntity.badRequest().body(Map.of("success", false, "message", "취소 실패: " + e.getMessage()));
         }
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<Map<String, Object>> createReview(@RequestBody PaymentReviewDTO reviewDTO) {
+        int result = paymentService.saveReview(reviewDTO);
+        Map<String, Object> response = new HashMap<>();
+        if (result > 0) {
+            PaymentpointDTO paymentpointDTO = paymentService.selectpoint();
+            paymentService.givepoint(paymentpointDTO);
+
+            response.put("success", true);
+            response.put("message", "리뷰가 성공적으로 저장되었습니다. \n리뷰감사 포인트 500p를 지급합니다.\n" + "보유포인트 : " + paymentpointDTO.getPossessionpoint() + "p");
+        } else {
+            response.put("success", false);
+            response.put("message", "리뷰 저장에 실패했습니다.");
+        }
+        return ResponseEntity.ok(response);
     }
 }
