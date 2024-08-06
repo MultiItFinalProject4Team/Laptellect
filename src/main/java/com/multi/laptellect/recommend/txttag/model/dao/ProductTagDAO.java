dbb404ee98cc4a2a78b8490f2c02ee6964b91bad
Youn Sang Hyun <younsang523@gmail.com>
2024-08-06 17:40:31 +0900

    feat : 태그 할당 테스트전
    api는 고침

--- src/main/java/com/multi/laptellect/recommend/txttag/model/dao/ProductTagDAO.java
+++ src/main/java/com/multi/laptellect/recommend/txttag/model/dao/ProductTagDAO.java
@@ -1,5 +1,8 @@
 package com.multi.laptellect.recommend.txttag.model.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ProductTagDAO {
     void deleteProductTags(int productNo);
     void insertProductTag(int productNo, int tagNo);
