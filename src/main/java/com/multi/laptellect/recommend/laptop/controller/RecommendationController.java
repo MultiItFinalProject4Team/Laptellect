c786e66a7832e4f3254f4e3974c399f6b3f79740
Youn Sang Hyun <younsang523@gmail.com>
2024-08-07 14:47:42 +0900

    feat : 임시 커밋
    커밋 고쳐야 할 곳 많음

--- src/main/java/com/multi/laptellect/recommend/laptop/controller/RecommendationController.java
+++ src/main/java/com/multi/laptellect/recommend/laptop/controller/RecommendationController.java
@@ -7,16 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
 
-
 @Controller
-@RequestMapping("/recommend")
 public class RecommendationController {
 
     private static final Logger logger = LoggerFactory.getLogger(RecommendationController.class);
@@ -24,18 +22,23 @@ public class RecommendationController {
     @Autowired
     private RecommendProductService recommendProductService;
 
-    @PostMapping
-    public String getRecommendations(@RequestBody Map<String, String> surveyResults, Model model) {
+    @GetMapping("/recommend")
+    public String showRecommendationForm() {
+        return "recommend/recommend";
+    }
+
+    @PostMapping("/recommendpage")
+    public String getRecommendations(@RequestParam Map<String, String> surveyResults, Model model) {
         logger.info("Received survey results: {}", surveyResults);
         try {
             List<RecommendProductDTO> recommendations = recommendProductService.getRecommendations(surveyResults);
-            model.addAttribute("recommendations", recommendations);
             logger.info("Found {} recommended products", recommendations.size());
-            return "recommendProductPage";
+            model.addAttribute("recommendations", recommendations);
+            return "recommend/recommendpage";
         } catch (Exception e) {
             logger.error("Error while processing recommendations", e);
             model.addAttribute("error", "An error occurred while processing your request.");
-            return "errorPage";
+            return "error";
         }
     }
 }
