065f26cbe198305bfeab9d5f77800948a0ad0337
Youn Sang Hyun <younsang523@gmail.com>
2024-08-17 19:40:13 +0900

    feat : 정기 커밋

--- src/main/java/com/multi/laptellect/config/Scheduler/SchedulerConfiguration.java
+++ src/main/java/com/multi/laptellect/config/Scheduler/SchedulerConfiguration.java
@@ -9,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 스케쥴러 설정 클래스
  *
@@ -25,53 +28,52 @@ public class SchedulerConfiguration {
     private final ProductService productService;
     private final RecommenService recommenService;
 
-//    @Scheduled(fixedRate = 50000) // fixedRate(서버 작동하자마자 시작) fixedDelay(종료 시점부터 시작, 1000 = 1초)
-//    public void run() {
-//        log.info("서버 시작 스케쥴러 확인");
-//    }
+    @Scheduled(fixedRate = 50000) // fixedRate(서버 작동하자마자 시작) fixedDelay(종료 시점부터 시작, 1000 = 1초)
+    public void run() {
+        log.info("서버 시작 스케쥴러 확인");
+    }
 
     @Scheduled(fixedRate = 300000) // 5분 간격으로 방문자 수 카운트
     public void visitCount() {
         log.info("방문자 수 카운트 스케쥴러");
-            String sessionKey = "Visit:" + "count";
-        int count = Integer.parseInt(redisUtil.getData(sessionKey));
+        String sessionKey = "Visit:" + "count";
 
         try {
+            int count = Integer.parseInt(redisUtil.getData(sessionKey));
             logService.insertVisitCount(count);
             redisUtil.deleteData(sessionKey);
         } catch (Exception e) {
-            log.error("방문자 없음 = {}", count);
+            log.error("방문자 없음");
         }
 
     }
-}
 
-//    @Scheduled(fixedRate = 180000) // 3분 간격으로 상품 조회수 업데이트
-//    public void viewProductCount() {
-//        String key = "Visit:product";
-//
-//        int visitCount = 0;
-//
-//        log.debug("상품 조회수 업데이트 시작");
-//        try {
-//            Map<String, String> visitProducts = redisUtil.getAllHashData(key);
-//            Set<String> productNos = visitProducts.keySet();
-//
-//            if(!visitProducts.isEmpty()) {
-//                for(String productNo : productNos) {
-//                    visitCount = Integer.parseInt(visitProducts.get(productNo));
-//                    log.info("상품 조회수 = {}, {}", productNo, visitCount);
-//
-//                    productService.updateProductVisit(productNo, visitCount);
-//                    log.info("상품 조회수 업데이트 완료 = {}", visitCount);
-//
-//                    redisUtil.deleteHashData(key, productNo);
-//                    log.info("상품 방문자 기록 삭제 완료 = {}", productNo);
-//                }
-//            }
-//        } catch (Exception e) {
-//            log.error("상품 조회수 업데이트 실패 = ", e);
-//        }
-//    }
-//
-//}
+
+    @Scheduled(fixedRate = 180000) // 3분 간격으로 상품 조회수 업데이트
+    public void viewProductCount() {
+        String key = "Visit:product";
+
+        int visitCount = 0;
+
+        log.debug("상품 조회수 업데이트 시작");
+        try {
+            Map<String, String> visitProducts = redisUtil.getAllHashData(key);
+            Set<String> productNos = visitProducts.keySet();
+
+            if (!visitProducts.isEmpty()) {
+                for (String productNo : productNos) {
+                    visitCount = Integer.parseInt(visitProducts.get(productNo));
+                    log.info("상품 조회수 = {}, {}", productNo, visitCount);
+
+                    productService.updateProductVisit(productNo, visitCount);
+                    log.info("상품 조회수 업데이트 완료 = {}", visitCount);
+
+                    redisUtil.deleteHashData(key, productNo);
+                    log.info("상품 방문자 기록 삭제 완료 = {}", productNo);
+                }
+            }
+        } catch (Exception e) {
+            log.error("상품 조회수 업데이트 실패 = ", e);
+        }
+    }
+}
