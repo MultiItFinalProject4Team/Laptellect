d2b26509f4e8ed2dce1dcee12ae6b6baa21c330a
ajw <anjy0821@naver.com>
2024-08-20 14:24:12 +0900

    style: css 수정

--- src/main/java/com/multi/laptellect/main/controller/MainController.java
+++ src/main/java/com/multi/laptellect/main/controller/MainController.java
@@ -1,27 +1,22 @@
 package com.multi.laptellect.main.controller;
 
-import com.multi.laptellect.main.service.MainService;
-import com.multi.laptellect.util.RedisUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MainController {
-    private final RedisUtil redisUtil;
-    private final MainService mainService;
+//    private final RedisUtil redisUtil;
+//    private final MainService mainService;
 
-    @GetMapping("/")
-    public String main(HttpSession httpSession, HttpServletRequest httpServletRequest) {
-        mainService.findLaptop();
-        mainService.findMouse();
-        mainService.findKeyboard();
-
-        return "common/main";
-    }
+//    @GetMapping("/")
+//    public String main(HttpSession httpSession, HttpServletRequest httpServletRequest) {
+//        mainService.findLaptop();
+//        mainService.findMouse();
+//        mainService.findKeyboard();
+//
+//        return "common/main";
+//    }
 }
