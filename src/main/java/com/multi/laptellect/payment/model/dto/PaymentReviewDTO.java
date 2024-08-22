637ad7aff4854dfc5410646eeca37e37a4b85cf4
이강석 <65838532+cpfur18@users.noreply.github.com>
2024-08-22 11:40:51 +0900

    fix : 리뷰
    1

--- src/main/java/com/multi/laptellect/payment/model/dto/PaymentReviewDTO.java
+++ src/main/java/com/multi/laptellect/payment/model/dto/PaymentReviewDTO.java
@@ -2,6 +2,8 @@ package com.multi.laptellect.payment.model.dto;
 
 import lombok.Data;
 
+import java.text.DecimalFormat;
+
 @Data
 public class PaymentReviewDTO {
     private int paymentProductReviewsNo;
@@ -16,4 +18,13 @@ public class PaymentReviewDTO {
     private String productName;
     private String memberName;
     private double avgRating;
+
+    public void setAvgRating(double avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public double getAvgRating() {
+        DecimalFormat df = new DecimalFormat("#.#");
+        return Double.parseDouble(df.format(avgRating));
+    }
 }
--- src/main/resources/static/js/member/delivery-profile.js
+++ src/main/resources/static/js/member/delivery-profile.js
@@ -21,7 +21,7 @@ $(function () {
 
         $("#address-name").on("blur", function () {
           let addressName = $(this).val().trim();
-          let regId = /[^a-zA-Z0-9]/g;
+          let regId = /[^a-zA-Z0-9가-힣]/g;
           console.log(addressName);
 
           if (addressName !== "") {
